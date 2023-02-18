ccc270d6ea2974e76167884cd0636c3bb49ea755
Gilbert Hada <gilberthada08@gmail.com>
2023-02-18 09:30:01 +0000

    working api

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+let port = 5000;
+
+const rappers = {
+  "chance the rapper": {
+    "age": 29,
+    "birthName": "Sheyaa Bin Abraham-Joseph",
+    "birthLocation": "London, England",
+  },
+  "21 savage": {
+    "age": 29,
+    "birthName": "Chancelor Bennett",
+    "birthLocation": "Chicago, Illinois",
+  },
+  "dylan": {
+    "age": 29,
+    "birthName": "unknown",
+    "birthLocation": "unknown",
+  },
+};
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:rapperName", (req, res) => {
+  let rappersName = req.params.rapperName.toLowerCase();
+  if (rappers[rappersName]) {
+    res.json(rappers[rappersName]);
+  } else {
+    res.json(rappers["dylan"]);
+  }
+  // res.json(rappers);
+});
+
+app.listen(port, function () {
+  console.log(`this server is running on port ${port}`);
+});
